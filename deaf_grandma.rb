2365f5e0dce45ce28a1988472345d612c0edbeb9
Bri Dotson <bridotson@gmail.com>
2014-09-07 15:16:43 -0700

    Fixed a bug with three BYEs

--- deaf_grandma.rb
+++ deaf_grandma.rb
@@ -7,13 +7,14 @@ while true
   if you_say == you_say.upcase && you_say != "BYE"
     n = 0
     puts "NO NOT SINCE #{rand(1930..1950)}"
-  else
-    if you_say == "BYE"
-      n += 1
-      if n == 3
-        exit
-      end
+  elsif you_say == "BYE"
+    n += 1
+    if n == 3
+      exit
     end
+    puts "HUH!? SPEAK UP, SONNY!"
+  else
+    n = 0
     puts "HUH!? SPEAK UP, SONNY!"
   end
 end
